fe20c8d80af6c4364d498a520437813e481a4b06
Davíð Örn Jóhannsson <davidoj@siminn.is>
2015-01-30 14:04:43 +0000

    removed unused variables in context.pp

--- manifests/config/context.pp
+++ manifests/config/context.pp
@@ -1,27 +1,14 @@
-# Definition: tomcat::config::server
+# Definition: tomcat::config::context
 #
-# Configure attributes for the Server element in $CATALINA_BASE/conf/server.xml
+# Configure attributes for the Context element in $CATALINA_BASE/conf/context.xml
 #
 # Parameters
 # - $catalina_base is the base directory for the Tomcat installation.
-# - $class_name is the optional className attribute.
-# - $class_name_ensure specifies whether you are trying to set or remove the
-#   className attribute. Valid values are 'true', 'false', 'present', or
-#   'absent'. Defaults to 'present'.
-# - $address is the optional address attribute.
-# - $address_ensure specifies whether you are trying to set of remove the
-#   address attribute. Valid values are 'true', 'false', 'present', or
-#   'absent'. Defaults to 'present'.
-# - The $port to wait for shutdown commands on.
-# - The $shutdown command that must be sent to $port.
+
+
+
 define tomcat::config::context (
   $catalina_base           = $::tomcat::catalina_home,
-  $class_name              = undef,
-  $class_name_ensure       = 'present',
-  $address                 = undef,
-  $address_ensure          = 'present',
-  $port                    = undef,
-  $shutdown                = undef,
 ) {
 
   if versioncmp($::augeasversion, '1.0.0') < 0 {
